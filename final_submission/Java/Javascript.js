5ff032544d3bc7f01f844264802b8c7650b26a0f
TrudyJo <jo.1981@yahoo.com>
2019-12-10 21:37:51 +0000

    HTML,CSS & JS updates

--- final_submission/Java/Javascript.js
+++ final_submission/Java/Javascript.js
@@ -1,35 +1,37 @@
-var currentIndex = 0,
-  items = $('.container div'),
-  itemAmt = items.length;
+//Slideshow Function
 
-function cycleItems() {
-  var item = $('.container div').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
+//Buttons
+
+var wordElement = ["pizza","teddy","milk","cocoa"]
+function clicked(){
+    console.log("I got this")
 }
 
-var autoSlide = setInterval(function() {
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-}, 3000);
+//Applying functions to specific HTML elements
 
-$('.next').click(function() {
-  clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-});
+let wordP = document.getElementById("wordElement")
+console.log(wordP)
 
-$('.prev').click(function() {
-  clearInterval(autoSlide);
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = itemAmt - 1;
-  }
-  cycleItems();
-});
+let displayWords = wordP.innerHTML
+console.log(displayWords)
+
+//Indexing
+function clicked(){
+   wordP.innerHTML= wordElement[3] //"Keep trying" 
+}
+
+let wordCounter = 0
+wordP.innerHTML = wordElement[wordCounter]
+
+function clicked(){
+    if (wordCounter>wordElement.length){
+        wordcounter=0
+    }
+    {wordCounter++
+    wordP.innerHTML = wordElement[wordCounter]}
+}
+console.log(wordCounter)
+
+//Buttons for PICs
+
+var pics =[""]
