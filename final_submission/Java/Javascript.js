5566774ad1263c29a5659a17174e6da4bef98366
TrudyJo <jo.1981@yahoo.com>
2019-12-11 15:20:17 +0000

    post coaching Updates

--- final_submission/Java/Javascript.js
+++ final_submission/Java/Javascript.js
@@ -1,58 +1,28 @@
 
 //cookies
-alert("This site uses cookies. By clicking OK you accept")
+//alert("This site uses cookies. By clicking OK you accept")
 
 //slideshow
 
-var img = 0; var path = ["cartoon1.jpeg","cartoon2.jpeg","cartoon3.jpeg"]; 
+var img = 0; var picElement = ["/Images/cartoon1.jpeg","./Images/cartoon2.jpeg","./Images/cartoon3.jpeg"]; 
 
-// LIST OF IMAGES 
-path[0] = "cartoon1.jpeg"; 
-path[1] = "cartoon2.jpeg"; 
-path[2] = "cartoon3.jpeg"; 
+let cartoons = document.getElementsByClassName("slidePics")
 
 function next(){
-document.getElementsByClassName()
-
-
-
-
-document.slide.src = path[img]; 
-
-if(img < path.length - 1) img++; 
-else img = 0; 
-setTimeout("swapImage()",3000); 
+    if(img < path.length - 1) 
+    img++; 
+    else img = 0;
+    img++ 
 } 
-window.onload=swapImage; 
 
-
-
-
-
-
-
-
+function prev(){
+    if(img < path.length - 1) 
+    img--; 
+    else img = 0;
+    img-- 
+} 
 
 //Contact Page Comment
 
-//ref: <body>  
-//   <div class="info">
-// <label for="example">Enter Text
-//    </label>
-//    <input id="example" type="text"
-//           name="Ntext" size="20">
-//    <input id="sent" type="submit"  
-//           value="Send"> 
-//</div>
-//  <p id="para">      
-//  </p>
-
-    window.onclick = function(e){
-                var id =  e.target.id;   
-                if (id === 'sent'){
-                var txt = document.getElementById('commentBox').value    
-                 $( "#para" ).empty().append( txt );
-                }
-             }
 
             
