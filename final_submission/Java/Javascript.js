fb3348a6208a8703b44a5ae1bec2ee8826516516
TrudyJo <jo.1981@yahoo.com>
2019-12-12 13:36:07 +0000

    commit on master

--- final_submission/Java/Javascript.js
+++ final_submission/Java/Javascript.js
@@ -1,42 +1,48 @@
 
 //cookies
-//alert("This site uses cookies. By clicking OK you accept")
+alert("This site uses cookies. By clicking OK you accept")
 
 //slideshow
 
-var slideIndex = 1;
-showSlides(slideIndex);
+//var slideIndex = 1;
+//showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+//function plusSlides(n) {
+ // showSlides(slideIndex += n);}
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+//function currentSlide(n) {
+//  showSlides(slideIndex = n);}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+//function showSlides(n) {
+  //var i;
+  //var slides = document.getElementsByClassName("mySlides");
+  //var dots = document.getElementsByClassName("demo");
+ // var captionText = document.getElementById("caption");
+ // if (n > slides.length) {slideIndex = 1}
+ // if (n < 1) {slideIndex = slides.length}
+  //for (i = 0; i < slides.length; i++) {
+ //   slides[i].style.display = "none";
+ // }
+ // for (i = 0; i < dots.length; i++) {
+ //   dots[i].className = dots[i].className.replace(" active", "");
+ // }
+ // slides[slideIndex-1].style.display = "block";
+ // dots[slideIndex-1].className += " active";
+ // captionText.innerHTML = dots[slideIndex-1].alt;}
  
 
 //Contact Page Comment
 
+//Get the url for where the data will come from
+var comdata = document.getElementById("commentBox").innerHTML
+userComments =[]
+
+function submit(){
+  comdata.push(userComments)
+}
+console.log(comdata);
+console.log(userComments)
 
             
