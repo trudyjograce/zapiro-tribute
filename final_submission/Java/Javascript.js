05b04a55513b631840c31530c93693fc95f043c1
TrudyJo <jo.1981@yahoo.com>
2019-12-13 09:57:24 +0000

    changes for error

--- final_submission/Java/Javascript.js
+++ final_submission/Java/Javascript.js
@@ -5,14 +5,21 @@ alert("This site uses cookies. By clicking OK you accept")
 //slideshow
 //code with coaching
 
+
+
 // Getting image tag from the DOM
+
+
+//Attributes can be manipulated on JS for the elements
+var images = ["images/cartoon1.jpg",
+              "images/cartoon2.jpg",
+              "images/cartoon3.jpg"];
+
+//Getting images from DOM
 let imgElement = document.getElementById("imagetest");
 let imgCounter = 0;
 
-//Attributes can be manipulated on JS for the elements
-var images = ["Images/cartoon1.jpg",
-              "Images/cartoon2.jpg",
-              "Images/cartoon3.jpg"];
+imgElement.src = images[imgCounter];
 
 //Next & Prev buttons
 
@@ -31,12 +38,11 @@ function click(){
 //Contact Page Comment
 
 //Get the url for where the data will come from
-var comdata = document.getElementById("commentBox").innerHTML
-let userComments =[]
+//var comdata = document.getElementById("commentBox").innerHTML
+//let userComments =[]
 
-function submit(){
-      comdata.push(userComments)
-}
+//function submit(){
+  //    comdata.push(userComments)}
 
 
             
